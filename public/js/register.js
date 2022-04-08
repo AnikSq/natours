e987fff11432b7a47b648e81309824a4f283f64d
AnikSq <anikr23456@gmail.com>
2022-04-08 10:33:47 +0530

    Prepared app for deployement

---
+++ public/js/register.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+import { showAlert } from './alerts';
+import axios from 'axios';
+
+export const register = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Created User Successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
